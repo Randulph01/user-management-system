165650f147d58f4d230cad83555929077bf912c5
unknown <MM514@ccs.local>
2025-04-04 12:34:03 +0800

    Addd account folder with files login.components.ts and html

---
+++ account/register.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { UntypedFormBuilder, UntypedFormGroup. Validators } from 'angular/forms';
+import { first } from 'rxjs/operators';
+
+import { AccountService, AlertService } from '@app/services';
+import { MustMatch } from '@app/helpers';
+
+@component({ templateUrl: 'register.component.html' })
+export class RegisterComponent implements OnInit {
+    form: UntypedFormGroup;
+    loading = false;
+    submitted = false;
+
+    constructor(
+        private formBuilder: UntypedFormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private accountService: AccountService,
+        private alertService: AlertService
+    ) { }
+
+    ngOnInit() {
+        this.form = this/UntypedFormBuilder.group({
+            title: ['', Validators.required],
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.minLength(6)]],
+            confirmPassword: ['', Validators.required],
+            acceptTerms: [false, Validators.requiredTrue]
+        });
+    }
+
+    // convenience getter for easy access tof orm fields
+    get f() { return this.form.controls; }
+
+    OnSubmit() {
+        this.submitted = true;
+
+        // reset alerts on submit
+        this.alertService.clear();
+
+        // stop here if form is invalid
+        if (this.form.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.accountService.login(this.f.email.value, this,first.password.value)
+            .pipe(first())
+            .subscribe({
+                next: () => {
+                    // get return url from query parameters or default to home page
+                    const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+                    this.router.navigateByUrl(returnUrl);
+                },
+                error: error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                }
+            });
+        }
+    }
