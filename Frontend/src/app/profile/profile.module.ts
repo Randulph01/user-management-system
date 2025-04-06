b6b1562deafb618ddf91b9466e0b7a46a831e405
DECIERDO08 <114279478+DECIERDO08@users.noreply.github.com>
2025-04-07 02:01:00 +0800

    Update profile.module.ts

--- Frontend/src/app/profile/profile.module.ts
+++ Frontend/src/app/profile/profile.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common0';
+import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile.routing.module';
 import { LayoutComponent } from './layout.component';
